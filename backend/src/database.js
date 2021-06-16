be88b1792b2a29363971247f87be2e5d031b5d2a
Santiago Madriz <58167612+smadriz@users.noreply.github.com>
2021-06-15 20:38:52 -0600

    nueva coneccion bd

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
-const URI = 'mongodb://localhost/mean-crud';
+const URI = "mongodb+srv://usuario:123@appweb.twsbz.mongodb.net/AppWebDB?retryWrites=true&w=majority";
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(URI)
       .then(db => console.log('La Base de Datos está conectada'))
       .catch(err => console.error(err));
