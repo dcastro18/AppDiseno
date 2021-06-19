27bbce57375ad001704252b6c408ef33f0ae194f
dcastro18 <h.davidcastro14@gmail.com>
2021-06-19 17:38:41 -0600

    cambios en orden de carpetas

--- frontend/GymApp/src/app/app.module.ts
+++ frontend/GymApp/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { CalendarioComponent } from './calendario/calendario.component';
-import { LeccionesComponent } from './lecciones/lecciones.component';
 import { VerClientesComponent } from './Instructores/ver-clientes/ver-clientes.component';
 import { AggLeccionComponent } from './Instructores/agg-leccion/agg-leccion.component';
 import { HomeClienteComponent } from './Clientes/home-cliente/home-cliente.component';
@@ -19,9 +16,6 @@ import { PagosClienteComponent } from './Clientes/pagos-cliente/pagos-cliente.co
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    CalendarioComponent,
-    LeccionesComponent,
     VerClientesComponent,
     AggLeccionComponent,
     HomeClienteComponent,
