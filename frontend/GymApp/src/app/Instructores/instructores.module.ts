0c21c0e6541995676d81f0249fd7d2ed379fed9f
dcastro18 <h.davidcastro14@gmail.com>
2021-06-21 17:58:33 -0600

    Inicio de instructor

---
+++ frontend/GymApp/src/app/Instructores/instructores.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InstructoresRoutingModule } from './instructores-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InstructoresRoutingModule
+  ]
+})
+export class InstructoresModule { }
